1e877c298c560ce37b24ad26c83f20f83053f544
xkball <3324786417@qq.com>
2022-10-01 12:32:22 +0800

    添加logo

--- src/main/java/com/xkball/hungry_in_peaceful_mode/mixin/MixinPlayer.java
+++ src/main/java/com/xkball/hungry_in_peaceful_mode/mixin/MixinPlayer.java
@@ -4,13 +4,12 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Player.class)
 public abstract class MixinPlayer {
     
     @Redirect(method = "aiStep",at = @At(value = "INVOKE",target = "Lnet/minecraft/world/food/FoodData;setFoodLevel(I)V"))
-    public void onPeacefulAIStep(FoodData instance, int pFoodLevel){
+    public void hipOnPeacefulAIStep(FoodData instance, int pFoodLevel){
     }
 }
